8ebb1cede72fefc3c5c575961fd50dba2ac967b8
André Zwing <nerv@dawncrow.de>
2024-12-29 21:28:54 +0100

    box64cpu: Add Box64 based library (v0.3.0)

---
+++ libs/box64/src/dynarec/arm64/dynarec_arm64_jmpnext.c
@@ -0,0 +1,13 @@
+#if defined(ARM64) || defined(__aarch64__)
+#include <stdint.h>
+
+#include "arm64_emitter.h"
+
+#define EMIT(A) *block = (A); ++block
+void CreateJmpNext(void* addr, void* next)
+{
+    uint32_t* block = (uint32_t*)addr;
+    LDRx_literal(x2, (intptr_t)next - (intptr_t)addr);
+    BR(x2);
+}
+#endif /* arm64 */
